d8fb53b806f4faefba4d71b61ab8e570fb9e8cc4
sureshgopal01 <38784775+sureshgopal01@users.noreply.github.com>
2018-08-06 21:20:25 +0530

    Update createextraction.component.ts

--- createextraction.component.ts
+++ createextraction.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ExtractionService } from '../../services/extraction.service';
 import { DatasourceService } from '../../services/datasource.service';
+import { TreeviewModule } from 'ngx-treeview';
 
 @Component({
   templateUrl: './createextraction.component.html'
@@ -16,16 +17,24 @@ export class CreateExtractionComponent implements OnInit {
   tables1: any;
   tables1Columns: any;
   dataSource: any;
+  checkBoxModel: any;
   selectedAll: any;
+  tables1SelectedColumns =  [];
+  selectedItems: any = [];
 
   constructor(private extractionService: ExtractionService, private dsService: DatasourceService,
     private router: Router,
     private activatedRoute: ActivatedRoute) { }
 
+
+    @Output('onSelect')
+    onSelect: EventEmitter<any> = new EventEmitter<any>();
+
   ngOnInit() {
     this.dataSource1_isChosen = false;
     console.log('Inside Create Extraction' );
     this.getDataSources();
+    // this.checkBoxModel = true;
   }
 
   getDataSources() {
@@ -93,11 +102,70 @@ export class CreateExtractionComponent implements OnInit {
       this.tables1Columns[i].selected = this.selectedAll;
     }
   }
-  
+
   checkIfAllSelected() {
+    // let selectedColumns: Array<any> = [];
     this.selectedAll = this.tables1Columns.every(function(item: any) {
+      // console.log('Selected columns = ' + item.name);
+      // this.tables1SelectedColumns.push(item.name);
+      // this.populateSelectedItems(item.selected);
         return item.selected === true;
       });
+      // this.tables1SelectedColumns = selectedColumns;
+      // console.log('Selected columns = ' + selectedColumns.toString);
+  }
+
+  populateSelectedItems(column) {
+
+    const index = this.tables1Columns.indexOf(column.name);
+    // if (event.target.checked) {
+        if (index === -1) {
+            this.selectedItems.push(column.name);
+        // }
+    } else {
+        if (index !== -1) {
+            this.selectedItems.splice(index, 1);
+        }
+    }
+    // console.log(this.selectedItems);
+  }
+
+  /*onItemSelect(item: any) {
+
+    const index = this.tables1Columns.indexOf(item.name);
+    if (index === -1) {
+      this.selectedItems.push(item.name);
+
+    } else  if (index !== -1) {
+      this.selectedItems.splice(index, 1);
+    }
+    // console.log(this.selectedItems);
+
+    console.log(item.name);
+    // this.selectedItems.push(item.name);
+    console.log(this.selectedItems);
+  }*/
+  onItemSelect(index, event) {
+    let selectedColumns: Array<any> = [];
+    // selectedColumns[index].value = event.target.checked;
+    console.log(index + ' ---- ' + event);
+    console.log('Column name is = ' + this.tables1Columns[index].name );
+    if (event === true) {
+      this.selectedItems.push(this.tables1Columns[index].name);
+    } else {
+      console.log('Removing Column name is = ' + this.tables1Columns[index].name );
+      this.selectedItems.splice(this.tables1Columns[index].name, 1);
+    }
+    /*this.tables1Columns.every(
+      function (item: any) {
+        console.log('Selected Col Name= ' + item.name);
+        console.log('Is Selected = ' + item.selected);
+        if (item.selected === true) {
+        console.log('Selected columns = ' + item.name);
+        selectedColumns.push(item.name);
+      }
+    });*/
+    console.log(this.selectedItems);
   }
 
 }
